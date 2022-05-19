607c8ac6ae1194967b6604a3370fdd0503ee2d80
harshini <harshinivutukuri1@gmail.com>
2022-05-19 10:01:38 +0530

    added multiple files

---
+++ Read-write.sol
@@ -0,0 +1,20 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.13;
+
+contract SimpleStorage {
+    // State variable to store a number
+    uint public num;
+
+    // You need to send a transaction to write to a state variable.
+    function set(uint _num) public {
+        num = _num;
+    }
+
+    // You can read from a state variable without sending a transaction.
+    function get() public view returns (uint) {
+        return num;
+    }
+}
+Writing or updating a state variable with transaction 
+Reading  from a state variable without sending a transaction.
+    
