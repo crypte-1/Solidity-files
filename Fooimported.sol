8e86095fa620e81853ba57a719174700e1ce20d7
cryptee.eth <105817423+crypte-1@users.noreply.github.com>
2022-05-24 23:02:16 +0530

    Update Fooimported.sol

--- Fooimported.sol
+++ Fooimported.sol
@@ -17,4 +17,9 @@ contract Import {
     }
 }
 
-//importing foo
+//Importing from github openzeppelin-contract repo
+
+// Example import ECDSA.sol from openzeppelin-contract repo, release-v4.5 branch
+// https://github.com/OpenZeppelin/openzeppelin-contracts/blob/release-v4.5/contracts/utils/cryptography/ECDSA.sol
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/release-v4.5/contracts/utils/cryptography/ECDSA.sol";
+
