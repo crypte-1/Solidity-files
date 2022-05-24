f4a720675d551745cf41732f936b5ba80fa45fba
cryptee.eth <105817423+crypte-1@users.noreply.github.com>
2022-05-24 22:59:33 +0530

    Create Import.sol

---
+++ Import.sol
@@ -0,0 +1,19 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.13;
+
+struct Point {
+    uint x;
+    uint y;
+}
+
+error Unauthorized(address caller);
+
+function add(uint x, uint y) pure returns (uint) {
+    return x + y;
+}
+
+contract Foo {
+    string public name = "Foo";
+}
+
+this is the main contract the other ones are importing this contract 
