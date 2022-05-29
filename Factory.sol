f53356fd53c57d81ef748fe88d8963c4d4b8aeb1
Harshini <105817423+crypte-1@users.noreply.github.com>
2022-05-29 11:47:12 +0530

    Update Factory.sol

--- Factory.sol
+++ Factory.sol
@@ -11,3 +11,10 @@ contract Child{
         data = _data;
      }
 }
+
+//The idea of the factory pattern is to have a contract (the factory) that will carry the mission of creating other contracts.
+
+There are two things needed when we want to call a deployed smart contract:
+
+    The ABI of the contract (provides info about functions signatures). If the contract is in the same project. you can just import it using the import keyword.
+    The address of the deployed contract.
